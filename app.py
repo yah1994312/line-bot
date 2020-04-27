8c9b487b811f8329408e679764399aa782053b3a
Gordon Chang <496929145@qq.com>
2020-04-27 11:36:23 +0800

    update

--- app.py
+++ app.py
@@ -38,10 +38,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃饭了吗'
+    r = '很抱歉，您说什么'
+
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '你吃饭了吗':
+        r = '还没'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
