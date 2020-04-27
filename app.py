0f129f69d026d9a4c0baa038b709d4db8a1357f6
Gordon Chang <496929145@qq.com>
2020-04-27 11:49:32 +0800

    update

--- app.py
+++ app.py
@@ -50,7 +50,10 @@ def handle_message(event):
         r = '您想订位，是吗？'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+    ))
 
 
 if __name__ == "__main__":
